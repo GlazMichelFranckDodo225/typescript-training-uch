c0b8d4361f659727a9850f1702bb8f91918f4b56
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-11 08:13:43 +0100

    Define a Constructor

--- 04-classes/01-class-definition/Customer.js
+++ 04-classes/01-class-definition/Customer.js
@@ -1,11 +1,14 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(first, last) {
+        this.firstName = first;
+        this.lastName = last;
     }
     return Customer;
 }());
 // Instance of Customer
-var myCustomer = new Customer();
-myCustomer.firstName = "John";
-myCustomer.lastName = "Doe";
+// let myCustomer = new Customer();
+/* myCustomer.firstName = "John";
+myCustomer.lastName = "Doe"; */
+var myCustomer = new Customer("John", "Doe");
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -1,13 +1,18 @@
 class Customer {
   firstName: string;
   lastName: string;
+
+  constructor(first: string, last: string) {
+    this.firstName=first;
+    this.lastName=last;
+  }
 }
 
 // Instance of Customer
-let myCustomer = new Customer();
-
-myCustomer.firstName = "John";
-myCustomer.lastName = "Doe";
+// let myCustomer = new Customer();
+/* myCustomer.firstName = "John";
+myCustomer.lastName = "Doe"; */
+let myCustomer = new Customer("John", "Doe");
 
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastName);
