3b0d0ed52c3da4ec05babb1a7248c16228e6ce85
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-11 08:08:30 +0100

    Define Customer Class

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,11 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+// Instance of Customer
+var myCustomer = new Customer();
+myCustomer.firstName = "John";
+myCustomer.lastName = "Doe";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,15 @@
+class Customer {
+  firstName: string;
+  lastName: string;
+}
+
+// Instance of Customer
+let myCustomer = new Customer();
+
+myCustomer.firstName = "John";
+myCustomer.lastName = "Doe";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
+
+
